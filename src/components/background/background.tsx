7ae3e877be30eb4951b2d4bd4a16e5d0ae46afe7
rizer18 <romanroskanuk74@gmail.com>
2025-11-06 12:59:51 +0100

    asdfdsa

--- src/components/background/background.tsx
+++ src/components/background/background.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import img from "../../../src/background.png"
+import img from "../../../public/background.png"
 import "./background.css"
 export function Background(){
     return (
