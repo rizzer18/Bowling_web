6809b68e3fa76430241e13b374e3ff560bfaf145
rizer18 <romanroskanuk74@gmail.com>
2025-11-06 10:13:19 +0100

    Changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,14 @@
 import type { Metadata } from "next";
 import { Merriweather } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 const RobotoSlabFont = Merriweather({
   variable: "--font-merriweather",
   subsets: ["latin"],
   weight: ["700"],
 });
 export const metadata: Metadata = {
-  title: "Bowlingový Club SV | Týniště nad Orlicí",
-   icons: {
-    icon: "/WhatsApp_Image_2025-11-03_at_13.54.41_1a26547b.jpg",
-  },
+   title: "Bowlingový Club SV | Týniště nad Orlicí",
   description:
     "Bowlingový Club SV – bowlingový klub v Týništi nad Orlicí. Nabízíme možnost zahrát si bowling a ping-pong. Vždy vás rádi uvítáme a poskytneme vám odpovídající služby. Máme také možnost organizovat různé akce.",
   keywords: [
@@ -23,17 +21,24 @@ export const metadata: Metadata = {
     "SV",
     "sv",
   ],
-  authors: [{ name: "Roman Roshkanyuk", url: "https://https://bowlingovyclubsv.cz" }],
+  authors: [
+    { name: "Roman Roshkanyuk", url: "https://bowlingovyclubsv.cz" },
+  ],
   creator: "Roman Roshkanyuk",
   robots: {
     index: true,
     follow: true,
   },
+  icons: {
+    icon: "/WhatsApp_Image_2025-11-03_at_13.54.41_1a26547b.jpg",
+    shortcut: "/WhatsApp_Image_2025-11-03_at_13.54.41_1a26547b.jpg",
+    apple: "/WhatsApp_Image_2025-11-03_at_13.54.41_1a26547b.jpg",
+  },
   openGraph: {
     title: "Bowlingový Club SV",
     description:
       "Bowlingový Club SV – bowlingový klub v Týništi nad Orlicí. Nabízíme možnost zahrát si bowling a ping-pong a organizovat různé akce.",
-    url: "https://https://bowlingovyclubsv.cz",
+    url: "https://bowlingovyclubsv.cz",
     siteName: "Bowlingový Club SV",
     images: [
       {
@@ -51,7 +56,9 @@ export const metadata: Metadata = {
     title: "Bowlingový Club SV",
     description:
       "Bowlingový Club SV – bowlingový klub v Týništi nad Orlicí. Nabízíme možnost zahrát si bowling a ping-pong a organizovat různé akce.",
-    images: ["https://bowlingovyclubsv.cz/WhatsApp_Image_2025-11-03_at_13.54.41_1a26547b.jpg"],
+    images: [
+      "https://bowlingovyclubsv.cz/WhatsApp_Image_2025-11-03_at_13.54.41_1a26547b.jpg",
+    ],
     creator: "@RomanRoshkanyuk",
   },
 };
@@ -62,7 +69,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="cs">
+       <head>
+        <Script
+          id="organization-schema"
+          type="application/ld+json"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              "name": "Bowlingový Club SV",
+              "url": "https://bowlingovyclubsv.cz",
+              "logo": "https://bowlingovyclubsv.cz/WhatsApp_Image_2025-11-03_at_13.54.41_1a26547b.jpg",
+            }),
+          }}
+        />
+      </head>
       <body className={`${RobotoSlabFont.variable} AAAAAA`}>{children}</body>
     </html>
   );
