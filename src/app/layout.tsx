542437f72c5b3bdc84884fdeb3d43ebd1f68e3c5
rizer18 <romanroskanuk74@gmail.com>
2025-11-09 22:01:38 +0100

    ertyuh

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,29 +39,28 @@ export const metadata: Metadata = {
     siteName: "Bowlingový Club SV",
     images: [
       {
-       url: "https://bowlingovyclubsv.cz/favicon.ico",
-       width: 600,
-       height: 600,
-       alt: "Bowlingový Club SV",
-     },
+        url: "https://bowlingovyclubsv.cz/logo512.png",
+        width: 600,
+        height: 600,
+        alt: "Bowlingový Club SV",
+      },
     ],
     locale: "cs_CZ",
     type: "website",
   },
-  
+
   twitter: {
     card: "summary_large_image",
     title: "Bowlingový Club SV",
     description:
       "Bowlingový Club SV – bowlingový klub v Týništi nad Orlicí. Nabízíme možnost zahrát si bowling a ping-pong a organizovat různé akce.",
     images: [
-       {
-       url: "https://bowlingovyclubsv.cz/favicon.ico",
-       width: 600,
-       height: 600,
-       alt: "Bowlingový Club SV",
-      
-     },
+      {
+        url: "https://bowlingovyclubsv.cz/logo512.png",
+        width: 600,
+        height: 600,
+        alt: "Bowlingový Club SV",
+      },
     ],
     creator: "@RomanRoshkanyuk",
   },
@@ -74,35 +73,35 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="cs">
-       <head>
-         <link rel="manifest" href="/manifest.json" />
-       <Script
-  id="organization-schema"
-  type="application/ld+json"
-  strategy="afterInteractive"
-  dangerouslySetInnerHTML={{
-    __html: JSON.stringify({
-      "@context": "https://schema.org",
-      "@type": "LocalBusiness",
-      "name": "Bowlingový Club SV",
-      "url": "https://bowlingovyclubsv.cz",
-      "logo": "https://bowlingovyclubsv.cz/favicon.ico",
-      "image": "https://bowlingovyclubsv.cz/favicon.ico",
-      "telephone": "+420607496833",
-      "address": {
-        "@type": "PostalAddress",
-        "streetAddress": "Nádražní 486",
-        "addressLocality": "Týniště nad Orlicí",
-        "postalCode": "517 21",
-        "addressCountry": "CZ"
-      },
-      "sameAs": [
-        "https://www.facebook.com/people/Bowlingov%C3%BD-Club-SV/61552814855837/?locale=cs_CZ#",
-        "https://www.instagram.com/bowlingovyklub/"
-      ]
-    }),
-  }}
-/>
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <Script
+          id="organization-schema"
+          type="application/ld+json"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "LocalBusiness",
+              name: "Bowlingový Club SV",
+              url: "https://bowlingovyclubsv.cz",
+              logo: "https://bowlingovyclubsv.cz/favicon.ico",
+              image: "https://bowlingovyclubsv.cz/favicon.ico",
+              telephone: "+420607496833",
+              address: {
+                "@type": "PostalAddress",
+                streetAddress: "Nádražní 486",
+                addressLocality: "Týniště nad Orlicí",
+                postalCode: "517 21",
+                addressCountry: "CZ",
+              },
+              sameAs: [
+                "https://www.facebook.com/people/Bowlingov%C3%BD-Club-SV/61552814855837/?locale=cs_CZ#",
+                "https://www.instagram.com/bowlingovyklub/",
+              ],
+            }),
+          }}
+        />
       </head>
       <body className={`${RobotoSlabFont.variable} AAAAAA`}>{children}</body>
     </html>
